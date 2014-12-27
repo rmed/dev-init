b606750c7e2c968bde09eaad4a27a6bdeb2c4419
Rafael Medina <rafamedgar@gmail.com>
2014-12-27 12:04:21 +0100

    Include version in source

--- dev_init/dev_init.py
+++ dev_init/dev_init.py
@@ -19,7 +19,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
-from _version import __version__
 import argparse
 import os
 import subprocess
@@ -31,6 +30,8 @@ except:
     from ConfigParser import ConfigParser
 if sys.version[0] == "3": raw_input=input
 
+
+__version__ = "0.2.1"
 
 CONFIG = os.path.join(os.path.expanduser("~"), ".dev-init")
 
